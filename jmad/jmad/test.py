d84367cdbaee004ec51a7b69eceb3a9b99dce35e
Francisco Manzano <keoh77@gmail.com>
2016-01-25 00:31:23 +0100

    Volviendo al libro, aunq travis pete

--- jmad/jmad/test.py
+++ jmad/jmad/test.py
@@ -19,59 +19,59 @@ class StudentTestCase(LiveServerTestCase):
         #examples of solos so he can improve his own
         #improvisation. He visits the home page of JMAD
 
-        #home_page = self.browser.get(self.live_server_url + '/')
+        home_page = self.browser.get(self.live_server_url + '/')
 
         #He knows he's in the right place because he can see
         #the name of the site in the heading
 
-        #brand_element = self.browser.find_element_by_css_selector('.navbar-brand')
-        #self.assertEqual('JMAD', brand_element.text)
+        brand_element = self.browser.find_element_by_css_selector('.navbar-brand')
+        self.assertEqual('JMAD', brand_element.text)
 
         #He sees the imputs of the search form, including
         #labels and placeholders
 
-        #instrument_input = self.browser.find_element_by_css_selector(
-        #    'input#jmad-instrument'
-        #)
+        instrument_input = self.browser.find_element_by_css_selector(
+            'input#jmad-instrument'
+        )
 
-        #self.assertIsNotNone(self.browser.find_element_by_css_selector(
-        #    'label[for="jmad-instrument"]'))
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'label[for="jmad-instrument"]'))
 
-        #self.assertEqual(instrument_input.get_attribute('placeholder'),
-        #    'i.e. trumpet')
+        self.assertEqual(instrument_input.get_attribute('placeholder'),
+            'i.e. trumpet')
 
-        #artist_input = self.browser.find_element_by_css_selector(
-        #    'input#jmad-artist')
+        artist_input = self.browser.find_element_by_css_selector(
+            'input#jmad-artist')
 
-        #self.assertIsNotNone(self.browser.find_element_by_css_selector(
-        #    'label[for="jmad-artist"]'))
+        self.assertIsNotNone(self.browser.find_element_by_css_selector(
+            'label[for="jmad-artist"]'))
 
-        #self.assertEqual(artist_input.get_attribute('placeholder'),
-        #    'i.e. Davis')
+        self.assertEqual(artist_input.get_attribute('placeholder'),
+            'i.e. Davis')
 
         #He types in the name of his instrument and submits
         #it.
-        #instrument_input.send_keys('saxophone')
-        #self.browser.find_element_by_css_selector('form button').click()
+        instrument_input.send_keys('saxophone')
+        self.browser.find_element_by_css_selector('form button').click()
         #He sees to many search results...
 
-        #search_results = self.browser.find_elements_by_css_selector(
-        #    '.jmad-search-result'
-        #)
+        search_results = self.browser.find_elements_by_css_selector(
+            '.jmad-search-result'
+        )
 
-        #self.assertGreater(len(search_results), 2)
+        self.assertGreater(len(search_results), 2)
 
 
         #...so he adds an artist to his search query and
         #gets a more manageable list.
 
-        #second_artist_input = self.browser.find_element_by_css_selector(
-        #    'input#jmad-artist')
-        #second_artist_input.send_keys('Cannonball Adderley')
-        #self.browser.find_element_by_css_selector('form button').click()
-        #second_search_results = self.browser.find_elements_by_css_selector(
-        #    '.jmad-search-result')
-        #self.assertEqual(len(second_search_results), 2)
+        second_artist_input = self.browser.find_element_by_css_selector(
+            'input#jmad-artist')
+        second_artist_input.send_keys('Cannonball Adderley')
+        self.browser.find_element_by_css_selector('form button').click()
+        second_search_results = self.browser.find_elements_by_css_selector(
+            '.jmad-search-result')
+        self.assertEqual(len(second_search_results), 2)
 
 
         self.fail('Incomplete Test')
