de932abd75d5906d77a17876a2c163fcbea9dd9d
Francisco Manzano <keoh77@gmail.com>
2016-01-25 03:02:16 +0100

    Adds a tag to search result, smal test refartor

--- jmad/jmad/test.py
+++ jmad/jmad/test.py
@@ -71,9 +71,7 @@ class StudentTestCase(LiveServerTestCase):
         self.browser.find_element_by_css_selector('form button').click()
         #He sees to many search results...
 
-        search_results = self.browser.find_elements_by_css_selector(
-            '.jmad-search-result'
-        )
+        search_results = self.find_search_results()
 
         self.assertGreater(len(search_results), 2)
 
@@ -85,8 +83,8 @@ class StudentTestCase(LiveServerTestCase):
             'input#jmad-artist')
         second_artist_input.send_keys('Cannonball Adderley')
         self.browser.find_element_by_css_selector('form button').click()
-        second_search_results = self.browser.find_elements_by_css_selector(
-            '.jmad-search-result')
+
+        second_search_results = self.find_search_results()
         self.assertEqual(len(second_search_results), 2)
 
         #He clicks on a search result.
@@ -94,7 +92,6 @@ class StudentTestCase(LiveServerTestCase):
 
         #The solo page has the title, artist and album for
         #this particular solo.
-        
 
         self.assertEqual(
             self.browser.current_url,
@@ -127,4 +124,9 @@ class StudentTestCase(LiveServerTestCase):
         self.assertEqual(
             self.browser.find_element_by_css_selector('#jmad-end-time').text,
             '4:01'
+        )
+
+    def find_search_results(self):
+        return self.browser.find_elements_by_css_selector(
+            '.jmad-search-result a'
         )
